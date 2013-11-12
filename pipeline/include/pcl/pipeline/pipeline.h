a95f4ca60a2f73903ff9a15c80ee0e0715c7c234
Bradley J Chambers <brad.chambers@gmail.com>
2013-11-12 09:15:16 -0500

    crude pipeline plumbing

--- pipeline/include/pcl/pipeline/pipeline.h
+++ pipeline/include/pcl/pipeline/pipeline.h
@@ -2,8 +2,9 @@
  * Software License Agreement (BSD License)
  *
  *  Point Cloud Library (PCL) - www.pointclouds.org
- *  Copyright (c) 2010-2013, Willow Garage, Inc.
- *  Copyright (c) 2013-, RadiantBlue Technologies, Inc.
+ *  Copyright (c) 2009-2012, Willow Garage, Inc.
+ *  Copyright (c) 2012-, Open Perception, Inc.
+ *  Copyright (c) 2014, RadiantBlue Technologies, Inc.
  *
  *  All rights reserved.
  *
@@ -90,7 +91,6 @@ namespace pcl
       {
         filename_ = filename;
       }
-
 
     protected:
       using PCLBase<PointT>::input_;
