b6bc638591bbd92fba84922f27254bf369563bfb
Bradley J Chambers <brad.chambers@gmail.com>
2014-05-19 21:48:12 -0400

    ugly first draft

---
+++ tools/progressive_densification.cpp
@@ -0,0 +1,504 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2009-2012, Willow Garage, Inc.
+ *  Copyright (c) 2012-, Open Perception, Inc.
+ *  Copyright (c) 2014, RadiantBlue Technologies, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#include <Eigen/Geometry>
+
+//#include <pcl/ros/conversions.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/console/print.h>
+#include <pcl/console/parse.h>
+#include <pcl/console/time.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/filters/crop_hull.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/grid_minimum.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/surface/gp3.h>
+
+using namespace std;
+using namespace pcl;
+using namespace pcl::io;
+using namespace pcl::console;
+
+typedef PointXYZ PointType;
+typedef PointCloud<PointXYZ> Cloud;
+typedef Cloud::Ptr CloudPtr;
+typedef const Cloud::ConstPtr ConstCloudPtr;
+
+int default_max_window_size = 33;
+float default_slope = 0.7f;
+float default_max_distance = 10.0f;
+float default_initial_distance = 0.15f;
+float default_cell_size = 1.0f;
+float default_base = 2.0f;
+bool default_exponential = true;
+int default_verbosity_level = 3;
+
+void
+printHelp (int, char **argv)
+{
+  print_error ("Syntax is: %s input.pcd output.pcd <options>\n", argv[0]);
+  print_info ("  where options are:\n");
+  print_info ("                     -max_window_size X = maximum window size (default: ");
+  print_value ("%d", default_max_window_size);
+  print_info (")\n");
+  print_info ("                     -slope X = slope value to compute threshold (default: ");
+  print_value ("%f", default_slope);
+  print_info (")\n");
+  print_info ("                     -max_distnace X = maximum distance from parameterized ground surface to be considered ground (default: ");
+  print_value ("%f", default_max_distance);
+  print_info (")\n");
+  print_info ("                     -initial_distance X = initial distance from parameterized ground surface to be considered ground (default: ");
+  print_value ("%f", default_initial_distance);
+  print_info (")\n");
+  print_info ("                     -cell_size X = cell size (default: ");
+  print_value ("%f", default_cell_size);
+  print_info (")\n");
+  print_info ("                     -base X = base to be used in computing progressive window sizes (default: ");
+  print_value ("%f", default_base);
+  print_info (")\n");
+  print_info ("                     -exponential X = use exponential growth? (default: ");
+  print_value ("%s", default_exponential?"true":"false");
+  print_info (")\n");
+  print_info ("                     -input_dir X  = batch process all PCD files found in input_dir\n");
+  print_info ("                     -output_dir X = save the processed files from input_dir in this directory\n");
+  print_info ("                     -verbosity X = verbosity level (default: ");
+  print_value ("%d", default_verbosity_level);
+  print_info (")\n");
+}
+
+bool
+loadCloud (const std::string &filename, Cloud &cloud)
+{
+  TicToc tt;
+  print_highlight ("Loading ");
+  print_value ("%s ", filename.c_str ());
+
+  tt.tic ();
+  if (loadPCDFile (filename, cloud) < 0)
+    return (false);
+  print_info ("[done, ");
+  print_value ("%g", tt.toc ());
+  print_info (" ms : ");
+  print_value ("%d", cloud.width * cloud.height);
+  print_info (" points]\n");
+  print_info ("Available dimensions: ");
+  print_value ("%s\n", getFieldsList (cloud).c_str ());
+
+  return (true);
+}
+
+void
+saveCloud (const std::string &filename, const Cloud &output)
+{
+  TicToc tt;
+  tt.tic ();
+
+  print_highlight ("Saving ");
+  print_value ("%s ", filename.c_str ());
+
+  PCDWriter w;
+  w.writeBinaryCompressed (filename, output);
+
+  print_info ("[done, ");
+  print_value ("%g", tt.toc ());
+  print_info (" ms : ");
+  print_value ("%d", output.width * output.height);
+  print_info (" points]\n");
+}
+
+void
+getAnglesToVertices (PointXYZ p, PointXYZ v1, PointXYZ v2, PointXYZ v3, float &angle1, float &angle2, float &angle3)
+{
+  Eigen::Vector3f vv1, vv2, n;
+  vv1[0] = v2.x - v1.x;
+  vv1[1] = v2.y - v1.y;
+  vv1[2] = v2.z - v1.z;
+  vv2[0] = v3.x - v1.x;
+  vv2[1] = v3.y - v1.y;
+  vv2[2] = v3.z - v1.z;
+  n[0] = vv1[1]*vv2[2] - vv1[2]*vv2[1];
+  n[1] = vv1[2]*vv2[0] - vv1[0]*vv2[2];
+  n[2] = vv1[0]*vv2[1] - vv1[1]*vv2[0];
+
+  Eigen::Vector3f l1, l2, l3;
+  l1[0] = p.x - v1.x;
+  l1[1] = p.y - v1.y;
+  l1[2] = p.z - v1.z;
+  l2[0] = p.x - v2.x;
+  l2[1] = p.y - v2.y;
+  l2[2] = p.z - v2.z;
+  l3[0] = p.x - v3.x;
+  l3[1] = p.y - v3.y;
+  l3[2] = p.z - v3.z;
+
+  angle1 = -std::asin ((n[0]*l1[0]+n[1]*l1[1]+n[2]*l1[2])/ (std::sqrt (n[0]*n[0]+n[1]*n[1]+n[2]*n[2])*std::sqrt (l1[0]*l1[0]+l1[1]*l1[1]+l1[2]*l1[2])));
+  angle2 = -std::asin ((n[0]*l2[0]+n[1]*l2[1]+n[2]*l2[2])/ (std::sqrt (n[0]*n[0]+n[1]*n[1]+n[2]*n[2])*std::sqrt (l2[0]*l2[0]+l2[1]*l2[1]+l2[2]*l2[2])));
+  angle2 = -std::asin ((n[0]*l3[0]+n[1]*l3[1]+n[2]*l3[2])/ (std::sqrt (n[0]*n[0]+n[1]*n[1]+n[2]*n[2])*std::sqrt (l3[0]*l3[0]+l3[1]*l3[1]+l3[2]*l3[2])));
+}
+
+void
+compute (ConstCloudPtr &input, Cloud &output, int max_window_size, float slope, float max_distance, float initial_distance, float cell_size, float base, bool exponential)
+{
+  // Estimate
+  TicToc tt;
+  tt.tic ();
+
+  print_highlight (stderr, "Computing ");
+  std::cerr << std::endl;
+
+  std::cerr << input->points.size () << std::endl;
+//  std::vector<int> ground;
+
+  CloudPtr cloud_out (new Cloud);
+  GridMinimum<PointXYZ> gm (10.0f);
+  gm.setInputCloud (input);
+  gm.filter (*cloud_out);
+
+  saveCloud ("gm.pcd", *cloud_out);
+
+  std::cerr << cloud_out->points.size () << std::endl;
+
+  // Normal estimation*
+  NormalEstimation<PointXYZ, Normal> n;
+  PointCloud<Normal>::Ptr normals (new PointCloud<Normal>);
+  search::KdTree<PointXYZ>::Ptr tree (new search::KdTree<PointXYZ>);
+  tree->setInputCloud (cloud_out);
+  n.setInputCloud (cloud_out);
+  n.setSearchMethod (tree);
+  n.setKSearch (20);
+  n.compute (*normals);
+  //* normals should not contain the point normals + surface curvatures
+
+  // Concatenate the XYZ and normal fields*
+  PointCloud<PointNormal>::Ptr cloud_with_normals (new PointCloud<PointNormal>);
+  concatenateFields (*cloud_out, *normals, *cloud_with_normals);
+  //* cloud_with_normals = cloud + normals
+
+  // Create search tree*
+  search::KdTree<PointNormal>::Ptr tree2 (new search::KdTree<PointNormal>);
+  tree2->setInputCloud (cloud_with_normals);
+
+  // Initialize objects
+  GreedyProjectionTriangulation<PointNormal> gp3;
+  PolygonMesh triangles;
+
+  // Set the maximum distance between connected points (maximum edge length)
+  gp3.setSearchRadius (100.0);
+
+  // Set typical values for the parameters
+  gp3.setMu (2.5);
+  gp3.setMaximumNearestNeighbors (100);
+  gp3.setMaximumSurfaceAngle (M_PI/4); // 45 degrees
+  gp3.setMinimumAngle (M_PI/18); // 10 degrees
+  gp3.setMaximumAngle (2*M_PI/3); // 120 degrees
+  gp3.setNormalConsistency (false);
+
+  // Get result
+  gp3.setInputCloud (cloud_with_normals);
+  gp3.setSearchMethod (tree2);
+  gp3.reconstruct (triangles);
+
+  // get the polygonmesh cloud
+  CloudPtr tri_cloud (new Cloud);
+  fromPCLPointCloud2 (triangles.cloud, *tri_cloud);
+
+  std::cerr << triangles.polygons.size () << std::endl;
+  std::cerr << triangles.polygons[0].vertices.size () << std::endl;
+  std::cerr << tri_cloud->points.size () << std::endl;
+  std::cerr << std::setprecision (2) << std::fixed;
+
+// getting vertices of first triangle
+  PointXYZ a = tri_cloud->points[triangles.polygons[0].vertices[0]];
+  PointXYZ b = tri_cloud->points[triangles.polygons[0].vertices[1]];
+  PointXYZ c = tri_cloud->points[triangles.polygons[0].vertices[2]];
+
+// getting the same vertices as a cloud for computing centroid
+  PointIndicesPtr idx (new PointIndices);
+  idx->indices.push_back (triangles.polygons[0].vertices[0]);
+  idx->indices.push_back (triangles.polygons[0].vertices[1]);
+  idx->indices.push_back (triangles.polygons[0].vertices[2]);
+  ExtractIndices<PointXYZ> extract;
+  extract.setInputCloud (tri_cloud);
+  extract.setIndices (idx);
+  CloudPtr tri (new Cloud);
+  extract.filter (*tri);
+
+  std::cerr << tri->points.size () << std::endl;
+
+// cropping input cloud to only those points within the first triangle
+  CropHull<PointXYZ> ch;
+  ch.setInputCloud (input);
+  ch.setHullCloud (tri_cloud);
+  ch.setDim (2);
+  std::vector<Vertices> first_triangle;
+  first_triangle.push_back (triangles.polygons[0]);
+  ch.setHullIndices (first_triangle);
+  std::vector<int> hidx;
+  ch.filter (hidx);
+  std::cerr << hidx.size () << " in hull" << std::endl;
+
+// find centroid of vertices
+  Eigen::Vector4f centroid;
+  compute3DCentroid (*tri, centroid);
+
+  std::cerr << centroid << std::endl;
+
+// get plane defined by vertices
+  Eigen::Hyperplane<float, 3> eigen_plane =
+    Eigen::Hyperplane<float, 3>::Through (a.getArray3fMap (),
+                                          b.getArray3fMap (),
+                                          c.getArray3fMap ());
+
+// compute distance between centroid and plane
+  PointXYZ d;
+  d.x = centroid[0];
+  d.y = centroid[1];
+  d.z = centroid[2];
+  float dist = eigen_plane.absDistance (d.getArray3fMap ()); // signedDistance also available, won't points always be above the plane anyway?
+
+  std::cerr << dist << std::endl;
+
+// find nearest point in cloud to centroid
+  search::KdTree<PointXYZ>::Ptr tree3 (new search::KdTree<PointXYZ>);
+  tree3->setInputCloud (input);
+  std::vector<int> indices (1);
+  std::vector<float> distances (1);
+  tree3->nearestKSearch (d, 1, indices, distances);
+  std::cerr << input->points[indices[0]] << std::endl;
+
+  // compute distance
+  std:: cerr << eigen_plane.absDistance (input->points[indices[0]].getArray3fMap ()) << std::endl;
+
+  // find angles between point and vertices
+  float angle1, angle2, angle3;
+  getAnglesToVertices (input->points[indices[0]], a, b, c, angle1, angle2, angle3);
+
+  std::cerr << angle1*180/M_PI << ", " << angle2*180/M_PI << ", " << angle3*180/M_PI << std::endl;
+
+  PointIndicesPtr addtoground (new PointIndices);
+
+  for (int i = 0; i < hidx.size (); ++i)
+  {
+    getAnglesToVertices (input->points[hidx[i]], a, b, c, angle1, angle2, angle3);
+    dist = eigen_plane.absDistance (input->points[hidx[i]].getArray3fMap ());
+    if (dist < 1.5 && angle1 < 6*M_PI/180 && angle2 < 6*M_PI/180 && angle3 < 6*M_PI/180)
+    {
+      std::cerr << dist << ", " << angle1*180/M_PI << ", " << angle2*180/M_PI << ", " << angle3*180/M_PI << std::endl;
+      std::cerr << input->points[hidx[i]] << std::endl;
+      addtoground->indices.push_back (hidx[i]);
+    }
+  }
+
+  std::cerr << addtoground->indices.size () << std::endl;
+
+  CloudPtr ground (new Cloud);
+  extract.setInputCloud (input);
+  extract.setIndices (addtoground);
+  extract.filter (*ground);
+  saveCloud ("densify.pcd", *ground);
+
+//  PointIndicesPtr idx (new PointIndices);
+//  idx->indices = ground;
+
+//  ExtractIndices<PointType> extract;
+//  extract.setInputCloud (input);
+//  extract.setIndices (idx);
+//  extract.setNegative (false);
+//  extract.filter (output);
+
+  print_info ("[done, ");
+  print_value ("%g", tt.toc ());
+  print_info (" ms : ");
+  print_value ("%d", output.width * output.height);
+  print_info (" points]\n");
+}
+
+int
+batchProcess (const vector<string> &pcd_files, string &output_dir, int max_window_size, float slope, float max_distance, float initial_distance, float cell_size, float base, bool exponential)
+{
+  vector<string> st;
+  for (size_t i = 0; i < pcd_files.size (); ++i)
+  {
+    // Load the first file
+    Cloud::Ptr cloud (new Cloud);
+    if (!loadCloud (pcd_files[i], *cloud))
+      return (-1);
+
+    // Perform the feature estimation
+    Cloud output;
+    compute (cloud, output, max_window_size, slope, max_distance, initial_distance, cell_size, base, exponential);
+
+    // Prepare output file name
+    string filename = pcd_files[i];
+    boost::trim (filename);
+    boost::split (st, filename, boost::is_any_of ("/\\"), boost::token_compress_on);
+
+    // Save into the second file
+    stringstream ss;
+    ss << output_dir << "/" << st.at (st.size () - 1);
+    //saveCloud (ss.str (), output);
+  }
+  return (0);
+}
+
+
+/* ---[ */
+int
+main (int argc, char** argv)
+{
+  print_info ("Filter a point cloud using the pcl::ProgressiveMorphologicalFilter. For more information, use: %s -h\n", argv[0]);
+
+  if (argc < 3)
+  {
+    printHelp (argc, argv);
+    return (-1);
+  }
+
+  bool batch_mode = false;
+
+  // Command line parsing
+  int max_window_size = default_max_window_size;
+  float slope = default_slope;
+  float max_distance = default_max_distance;
+  float initial_distance = default_initial_distance;
+  float cell_size = default_cell_size;
+  float base = default_base;
+  bool exponential = default_exponential;
+  int verbosity_level = default_verbosity_level;
+  parse_argument (argc, argv, "-max_window_size", max_window_size);
+  parse_argument (argc, argv, "-slope", slope);
+  parse_argument (argc, argv, "-max_distance", max_distance);
+  parse_argument (argc, argv, "-initial_distance", initial_distance);
+  parse_argument (argc, argv, "-cell_size", cell_size);
+  parse_argument (argc, argv, "-base", base);
+  parse_argument (argc, argv, "-exponential", exponential);
+  parse_argument (argc, argv, "-verbosity", verbosity_level);
+  string input_dir, output_dir;
+  if (parse_argument (argc, argv, "-input_dir", input_dir) != -1)
+  {
+    PCL_INFO ("Input directory given as %s. Batch process mode on.\n", input_dir.c_str ());
+    if (parse_argument (argc, argv, "-output_dir", output_dir) == -1)
+    {
+      PCL_ERROR ("Need an output directory! Please use -output_dir to continue.\n");
+      return (-1);
+    }
+
+    // Both input dir and output dir given, switch into batch processing mode
+    batch_mode = true;
+  }
+
+  switch (verbosity_level)
+  {
+    case 0:
+      setVerbosityLevel (L_ALWAYS);
+      break;
+
+    case 1:
+      setVerbosityLevel (L_ERROR);
+      break;
+
+    case 2:
+      setVerbosityLevel (L_WARN);
+      break;
+
+    case 3:
+      setVerbosityLevel (L_INFO);
+      break;
+
+    case 4:
+      setVerbosityLevel (L_DEBUG);
+      break;
+
+    default:
+      setVerbosityLevel (L_VERBOSE);
+      break;
+  }
+
+  if (!batch_mode)
+  {
+    // Parse the command line arguments for .pcd files
+    std::vector<int> p_file_indices;
+    p_file_indices = parse_file_extension_argument (argc, argv, ".pcd");
+    if (p_file_indices.size () != 2)
+    {
+      print_error ("Need one input PCD file and one output PCD file to continue.\n");
+      return (-1);
+    }
+
+    // Load the first file
+    Cloud::Ptr cloud (new Cloud);
+    if (!loadCloud (argv[p_file_indices[0]], *cloud))
+      return (-1);
+
+    // Perform the feature estimation
+    Cloud output;
+    compute (cloud, output, max_window_size, slope, max_distance, initial_distance, cell_size, base, exponential);
+
+    // Save into the second file
+    //saveCloud (argv[p_file_indices[1]], output);
+  }
+  else
+  {
+    if (input_dir != "" && boost::filesystem::exists (input_dir))
+    {
+      vector<string> pcd_files;
+      boost::filesystem::directory_iterator end_itr;
+      for (boost::filesystem::directory_iterator itr (input_dir); itr != end_itr; ++itr)
+      {
+        // Only add PCD files
+        if (!is_directory (itr->status ()) && boost::algorithm::to_upper_copy (boost::filesystem::extension (itr->path ())) == ".PCD")
+        {
+          pcd_files.push_back (itr->path ().string ());
+          PCL_INFO ("[Batch processing mode] Added %s for processing.\n", itr->path ().string ().c_str ());
+        }
+      }
+      batchProcess (pcd_files, output_dir, max_window_size, slope, max_distance, initial_distance, cell_size, base, exponential);
+    }
+    else
+    {
+      PCL_ERROR ("Batch processing mode enabled, but invalid input directory (%s) given!\n", input_dir.c_str ());
+      return (-1);
+    }
+  }
+}
+
